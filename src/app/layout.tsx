2b01b935e955abf2edbd46f24123b4624d2a580f
sorenblank <afrozaman12@gmail.com>
2024-06-14 05:30:01 +0600

    feat: Updated `metadata` for Cerebromesh Labs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,38 @@ import type { Metadata } from "next";
 import { sora, sourceCodePro } from "@/styles/fonts";
 import "@/styles/globals.css";
 
+export const metadata: Metadata = {
+  metadataBase: new URL("https://cerebromeshlabs.vercel.app"),
+  title: "CerebroMesh Labs",
+  description: "Cerebromesh Labs provides high-quality, curated datasets to enhance your AI training. Unlock your AI's potential with our expert data.",
+  openGraph: {
+    title: "CerebroMesh Labs",
+    description: "Cerebromesh Labs provides high-quality, curated datasets to enhance your AI training. Unlock your AI's potential with our expert data.",
+    type: "website",
+    locale: "en_US",
+    url: new URL("/", "https://cerebromeshlabs.vercel.app"),
+    images: [
+      {
+        url: "https://cerebromeshlabs.vercel.app/og-image.png",
+        width: 1784,
+        height: 892,
+        alt: "CerebroMesh Labs Open Graph Image"
+      }
+    ]
+  },
+  twitter: {
+    creator: "@CerebromeshLabs",
+    site: "@CerebromeshLabs",
+    card: "summary_large_image",
+    images: [
+      {
+        url: "https://cerebromeshlabs.vercel.app/og-image.png",
+        alt: "CerebroMesh Labs Twitter Card Image"
+      }
+    ]
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{

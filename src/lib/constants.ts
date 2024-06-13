cd336684e02e49cfbeb429babdc685ca1b2a0b21
sorenblank <afrozaman12@gmail.com>
2024-06-14 05:35:01 +0600

    feat: Added constants for `navigation`

---
+++ src/lib/constants.ts
@@ -0,0 +1,39 @@
+export const navigation = [
+    {
+      id: "0",
+      title: "Home",
+      url: "#home",
+    },
+    {
+      id: "1",
+      title: "Products",
+      url: "#products",
+    },
+    {
+      id: "2",
+      title: "Leaderboard",
+      url: "#leaderboard",
+    },
+    {
+      id: "3",
+      title: "Contact",
+      url: "#contact",
+    },
+    {
+      id: "4",
+      title: "Blog",
+      url: "#blog",
+    },
+    {
+      id: "5",
+      title: "New account",
+      url: "#signup",
+      onlyMobile: true,
+    },
+    {
+      id: "6",
+      title: "Sign in",
+      url: "#login",
+      onlyMobile: true,
+    },
+  ];  

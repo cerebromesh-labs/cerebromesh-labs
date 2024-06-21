65d811c58491b0ba7274a7790a5c950dcbfe5788
sorenblank <afrozaman12@gmail.com>
2024-06-21 11:40:57 +0600

    feat: Added `<Header/>`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Header from "@/components/layout/Header";
 
 export default function Home() {
   return (
-    <main>
-      CerebroMesh Labs
+    <main className="h-[200vh] bg-white">
+      <Header />
     </main>
   );
 }

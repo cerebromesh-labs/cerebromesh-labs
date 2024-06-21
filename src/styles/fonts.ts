9a356e217843730dc4fb4efce5e58cfb55003b56
sorenblank <afrozaman12@gmail.com>
2024-06-21 11:41:29 +0600

    feat: Updated `fonts.ts`

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,6 +1,7 @@
-import { Sora, Source_Code_Pro } from "next/font/google";
+import { Sora, Source_Code_Pro, Inter } from "next/font/google";
 
 const sora = Sora({ subsets: ["latin"], variable: "--font-sora" });
 const sourceCodePro = Source_Code_Pro({ subsets: ["latin"], variable: "--font-source-code-pro" });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
-export { sora, sourceCodePro };
+export { sora, sourceCodePro, inter };

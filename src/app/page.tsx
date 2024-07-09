54feebe610215ae3455137185694e40c20f2e02e
Taher Ahmed <taheirahmedashraf@gmail.com>
2024-07-09 20:19:31 +0600

    Get Started Section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/layout/Header";
+import GetStarted from "@/components/home/GetStarted";
 
 export default function Home() {
   return (
-    <main className="h-[200vh] bg-white">
+    <main className="min-h-[100vh] bg-main-bg text-white">
       <Header />
+      <GetStarted/>
     </main>
   );
 }

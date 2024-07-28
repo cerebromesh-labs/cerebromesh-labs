98fb4a6a3ce05153c576ed82a1c965b59e2a4f2b
sorenblank <afrozaman12@gmail.com>
2024-07-28 22:18:52 +0600

    refactor: Refactored and formatted `src/components/home/GetStarted.tsx` component

--- src/components/home/GetStarted.tsx
+++ src/components/home/GetStarted.tsx
@@ -1,22 +1,38 @@
 'use client'
 
 import React from 'react'
-
-
-import StarFieldGif from '@/assets/Starfield.gif'
 import Image from "next/image";
 import { FaChevronRight } from "react-icons/fa";
+import StarFieldGif from '@/assets/Starfield.gif'
 
-
-export default function GetStarted() {
-    return (
-        <div className={'w-full h-[100vh] relative'}>
-            <Image className={'absolute w-full h-full -z-20'} src={StarFieldGif} alt={'Star Field Gif'} unoptimized/>
-            <div className={'z-20 text-white flex flex-col justify-center items-center w-full h-full gap-6 px-4'}>
-                <h1 className={'lg:text-8xl md:text-7xl text-5xl font-medium text-center'}>Refined <span className={'text-primary'}>AI</span> Datasets</h1>
-                <p className={'lg:text-2xl md:text-xl text-lg  max-w-screen-md text-center '}>Cerebromesh Labs provides high-quality, curated datasets to enhance your AI training. We use the algorithm of <a href={'#'} className={'text-primary italic'}>huggingface.co</a></p>
-                <button  className={'bg-primary px-8 py-2 text-main-bg rounded-md text-lg font-semibold flex flex-row justify-center items-center gap-1'}>Get Started <FaChevronRight/></button>
-            </div>
-        </div>
-    )
+/**
+ * GetStarted component
+ * 
+ * This component renders the hero section of the website, including a background image,
+ * main heading, description, and a call-to-action button.
+ */
+const GetStarted: React.FC = () => {
+  return (
+    <div className="w-full h-[100vh] relative">
+      <Image
+        className="absolute w-full h-full -z-20"
+        src={StarFieldGif}
+        alt="Star Field Background"
+      />
+      <div className="z-20 text-white flex flex-col justify-center items-center w-full h-full gap-6 px-4">
+        <h1 className="lg:text-8xl md:text-7xl text-5xl font-medium text-center">
+          Refined <span className="text-primary">AI</span> Datasets
+        </h1>
+        <p className="lg:text-2xl md:text-xl text-lg max-w-screen-md text-center">
+          Cerebromesh Labs provides high-quality, curated datasets to enhance your AI training.
+          We use the algorithm of <a href="#" className="text-primary italic">huggingface.co</a>
+        </p>
+        <button className="bg-primary px-8 py-2 text-main-bg rounded-md text-lg font-semibold flex flex-row justify-center items-center gap-1">
+          Get Started <FaChevronRight />
+        </button>
+      </div>
+    </div>
+  )
 }
+
+export default GetStarted;

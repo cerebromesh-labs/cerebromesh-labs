7d41b5ec0882325061450d631740acbf7db7df5c
sorenblank <afrozaman12@gmail.com>
2024-07-28 22:16:52 +0600

    refactor: Refactored and formatted `page.tsx`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+// File: src/app/page.tsx
+
 import Header from "@/components/layout/Header";
 import GetStarted from "@/components/home/GetStarted";
 import GoodDataComp from "@/components/home/GoodData";
@@ -9,29 +11,31 @@ import EvaluationMethodology from "@/components/home/EvaluationMethodology";
 import Datasets from "@/components/home/Datasets";
 import Leaderboard from "@/components/home/Leaderboard";
 import Footer from "@/components/Footer";
-
 import FinewebRecipe from "@/components/home/FinewebRecipe";
-import StarBackground from '../../public/star-background.png'
-import Image from "next/image";
 
-
+/**
+ * Home component representing the main page of the application.
+ */
 export default function Home() {
   return (
-    <main className={'overflow-hidden'}>
+    <main className="overflow-hidden">
       <Header />
-      <GetStarted/>
-      <div className={'relative'}>
-          <div className={'absolute top-0 left-0 right-0 bottom-0 w-full -z-10 star-image opacity-60 '}/>
-          <GoodDataComp/>
-          <FinewebRecipe/>
-          <TextExtAlgo/>
-          <BaseFilteringAlgorithm/>
-          <DeduplicationAlgo/>
-          <QualityFilteringAlgo/>
-          <EvaluationMethodology/>
-          <Datasets/>
-          <Leaderboard/>
-          <Footer/>
+      <GetStarted />
+      <div className="relative">
+        {/* Background image */}
+        <div className="absolute inset-0 w-full -z-10 star-image opacity-60" />
+        
+        {/* Main content sections */}
+        <GoodDataComp />
+        <FinewebRecipe />
+        <TextExtAlgo />
+        <BaseFilteringAlgorithm />
+        <DeduplicationAlgo />
+        <QualityFilteringAlgo />
+        <EvaluationMethodology />
+        <Datasets />
+        <Leaderboard />
+        <Footer />
       </div>
     </main>
   );

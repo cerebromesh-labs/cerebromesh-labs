b783ebd2915cd0043d8ea5554c6812e63dcf5b79
sorenblank <afrozaman12@gmail.com>
2024-07-28 22:19:03 +0600

    refactor: Refactored and formatted `src/components/home/GoodData.tsx` component

--- src/components/home/GoodData.tsx
+++ src/components/home/GoodData.tsx
@@ -1,31 +1,34 @@
-
 'use client'
 
 import React from 'react'
-import DotImage from '@/assets/cerebromesh_inside_dot.png'
-import Image from "next/image";
+import { motion } from 'framer-motion';
 import SectionHeader from "@/components/home/SectionHeader";
 
-import { easeInOut, motion } from 'framer-motion';
-
-const fadeIn = {
+/**
+ * GoodDataComp component
+ * 
+ * This component displays information about understanding good data
+ * in the context of training large-scale models.
+ */
+const GoodDataComp: React.FC = () => {
+  const fadeIn = {
     initial: { opacity: 0 },
     animate: { opacity: 1 },
-    transition: { duration: .5, easeInOut }
-};
+    transition: { duration: 0.5, ease: "easeInOut" }
+  };
 
-const fadeInParagraph = {
-    initial: { opacity: 0 },
-    animate: { opacity: 1 },
-    transition: { duration: 2, easeInOut, delay: 0.5 }
-};
+  const description = 'Good data" is pivotal for training large-scale models, yet its quality can be ambiguous and context-specific. Human evaluation may miss subtle flaws. An effective approach involves training models on trusted datasets and evaluating new data with metrics like perplexity. This method ensures data reliability for algorithm training.';
 
-export default function GoodDataComp(){
-    return (
-        <div className={'container mx-auto py-8 '}>
-            <motion.div {...fadeIn}  className={'bg-secondary-bg rounded-lg lg:p-6 py-16 px-4'}>
-                <SectionHeader title={"Understanding Good Data"} description={'Good data" is pivotal for training large-scale models, yet its quality can be ambiguous and context-specific. Human evaluation may miss subtle flaws. An effective approach involves training models on trusted datasets and evaluating new data with metrics like perplexity. This method ensures data reliability for algorithm training. '}/>
-            </motion.div>
-        </div>
-    )
+  return (
+    <div className='container mx-auto py-8'>
+      <motion.div {...fadeIn} className='bg-secondary-bg rounded-lg lg:p-6 py-16 px-4'>
+        <SectionHeader
+          title="Understanding Good Data"
+          description={description}
+        />
+      </motion.div>
+    </div>
+  )
 }
+
+export default GoodDataComp;
